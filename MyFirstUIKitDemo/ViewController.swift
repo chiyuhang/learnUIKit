25731bb83cff810eacf77008ab95bde1c187b40d
chiyuhang <chiyuhang@bytedance.com>
2022-05-19 22:29:00 +0800

    Initial Commit

---
+++ MyFirstUIKitDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstUIKitDemo
+//
+//  Created by ByteDance on 2022/5/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
