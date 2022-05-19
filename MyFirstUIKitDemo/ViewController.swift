bec1f73509fb977a02061caedade5f24185f8dec
chiyuhang <chiyuhang@bytedance.com>
2022-05-19 23:34:17 +0800

    use label to print hello world

--- MyFirstUIKitDemo/ViewController.swift
+++ MyFirstUIKitDemo/ViewController.swift
@@ -11,7 +11,9 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print("Hello World ")
         // Do any additional setup after loading the view.
+        
     }
 
 
